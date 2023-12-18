6f01d09cfd008f54925d46ac871e40d5d3fdaf0a
Ivan Ivanov <iivan0989@gmail.com>
2023-12-18 19:36:17 +0900

    start

--- src/game/dto/create-game.dto.ts
+++ src/game/dto/create-game.dto.ts
@@ -2,7 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, IsUUID, IsJSON, IsNumber} from 'class-validator';
 
 export class CreateGameDto {
-  createGameDto: import("d:/4_curse/UNO_PROJECT/src/card/dto/create-card.dto").CreateCardDto;
     @IsNumber()
     @IsNotEmpty()
     @ApiProperty()
